f4503bf4d34b2ba574f46407ad3566c56e465abd
hristo-darlyanov <83130557+hristo-darlyanov@users.noreply.github.com>
2023-08-22 15:49:55 +0300

    Added modal for log out button in settings

---
+++ components/LeftPointingArrow.js
@@ -0,0 +1,16 @@
+import { default as IconAntDesign } from 'react-native-vector-icons/AntDesign';
+
+const LeftPointingArrow = () => {
+    return (
+        <IconAntDesign
+            name="right"
+            size={27}
+            backgroundColor={'black'}
+            color="white"
+            style={{ marginRight: 5 }}
+            onPress={() => { }}
+        />
+    )
+}
+
+export default LeftPointingArrow
--- screens/mainStackScreens/SettingsScreen.js
+++ screens/mainStackScreens/SettingsScreen.js
@@ -1,24 +1,58 @@
-import { ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import { ScrollView, StyleSheet, Text, TouchableOpacity, View, Modal, Pressable } from 'react-native'
 import { default as IconAntDesign } from 'react-native-vector-icons/AntDesign';
 import { default as IconMaterialCommunityIcons } from 'react-native-vector-icons/MaterialCommunityIcons';
-import React from 'react'
+import { signOut } from 'firebase/auth';
+import { auth } from '../../config/Firebase';
+import LeftPointingArrow from '../../components/LeftPointingArrow';
+import React, { useState } from 'react'
 
 const SettingsScreen = () => {
-    function LeftPointingArrow() {
+    const [modalVisible, setModalVisible] = useState(false);
+
+    const handleSignOut = () => {
+        signOut(auth).then(() => {
+            console.log("Sign out successful")
+        }).catch((error) => {
+            console.log(error.message)
+        });
+    }
+
+    function LogoutModal() {
         return (
-            <IconAntDesign
-                name="right"
-                size={27}
-                backgroundColor={'black'}
-                color="white"
-                style={{ marginRight: 5 }}
-                onPress={() => { }}
-            />
+            <Modal
+                animationType="fade"
+                transparent={true}
+                visible={modalVisible}
+                onRequestClose={() => {
+                    Alert.alert('Modal has been closed.');
+                    setModalVisible(!modalVisible);
+                }}>
+                <View style={styles.centeredView}>
+                    <View style={styles.modalView}>
+                        <View style={styles.modalDescriptionTextWrapper}>
+                            <Text style={styles.modalDescriptionText}>Are you sure you want to log out? You can always log in later</Text>
+                        </View>
+                        <View style={styles.modalButtonWrapper}>
+                            <TouchableOpacity
+                                style={styles.modalLogoutButton}
+                                onPress={() => setModalVisible(!modalVisible)}>
+                                <Text style={styles.modalTextStyle}>Log out</Text>
+                            </TouchableOpacity>
+                            <TouchableOpacity
+                                style={styles.modalCloseButton}
+                                onPress={() => setModalVisible(!modalVisible)}>
+                                <Text style={styles.modalTextStyle}>Close</Text>
+                            </TouchableOpacity>
+                        </View>
+                    </View>
+                </View>
+            </Modal>
         )
     }
 
     return (
         <View style={styles.container}>
+            <LogoutModal />
             <ScrollView contentContainerStyle={styles.contentWrapper}>
                 <Text style={styles.titleText}>Settings</Text>
 
@@ -68,7 +102,7 @@ const SettingsScreen = () => {
                     <Text style={styles.buttonText}>About</Text>
                     <LeftPointingArrow />
                 </TouchableOpacity>
-                <TouchableOpacity style={styles.button}>
+                <TouchableOpacity style={styles.button} onPress={() => setModalVisible(true)}>
                     <Text style={styles.buttonText}>Log out</Text>
                     <LeftPointingArrow />
                 </TouchableOpacity>
@@ -127,5 +161,61 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         alignItems: 'center',
         marginTop: '2%'
+    },
+    centeredView: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    modalView: {
+        backgroundColor: 'black',
+        borderRadius: 10,
+        borderColor: 'grey',
+        borderWidth: 1,
+        width: '90%',
+        height: '20%',
+        alignItems: 'center',
+        shadowColor: '#000',
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+        elevation: 5,
+    },
+    modalLogoutButton: {
+        borderRadius: 10,
+        paddingVertical: 10,
+        width: '47%',
+        backgroundColor: 'red'
+    },
+    modalCloseButton: {
+        borderRadius: 10,
+        paddingVertical: 10,
+        width: '47%',
+        backgroundColor: 'grey'
+    },
+    modalButtonWrapper: {
+        flexDirection: 'row',
+        position: 'absolute',
+        bottom: 0,
+        justifyContent: 'space-between',
+        marginBottom: 10
+    },
+    modalTextStyle: {
+        color: 'white',
+        fontWeight: '400',
+        textAlign: 'center',
+        fontSize: 20
+    },
+    modalDescriptionText: {
+        color: 'white',
+        fontSize: 22,
+        textAlign: 'center',
+        marginTop: '7%',
+    },
+    modalDescriptionTextWrapper: {
+        width: '90%'
     }
 })
