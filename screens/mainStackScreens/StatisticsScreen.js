a61202d7e00a1b86a6b595495142905a95d84131
hristo-darlyanov <83130557+hristo-darlyanov@users.noreply.github.com>
2023-09-28 15:44:20 +0300

    Added selection of agents and displaying profit graph

--- screens/mainStackScreens/StatisticsScreen.js
+++ screens/mainStackScreens/StatisticsScreen.js
@@ -7,34 +7,17 @@ import { query, collection, where, onSnapshot } from 'firebase/firestore';
 import { Dropdown } from 'react-native-element-dropdown';
 import { default as IconAntDesign } from 'react-native-vector-icons/AntDesign';
 
-const data = [
-  {
-    timestamp: 1625945400000,
-    value: 33575.25,
-  },
-  {
-    timestamp: 1625946300000,
-    value: 33545.25,
-  },
-  {
-    timestamp: 1625947200000,
-    value: 33510.25,
-  },
-  {
-    timestamp: 1625948100000,
-    value: 33215.25,
-  },
-];
-
 const StatisticsScreen = () => {
   const [tradesData, setTradesData] = useState([])
   const [agentData, setAgentData] = useState([])
-  const [dropdownData, setDropdownData] = useState([{ label: 'All', value: 'all' }])
-  const [value, setValue] = useState(null);
+  const [dropdownData, setDropdownData] = useState([{ label: 'All agents', value: 'all' }])
+  const [value, setValue] = useState("all");
   const [isFocus, setIsFocus] = useState(false);
+  const [lineData, setLineData] = useState([{ x: 0, value: 0 }])
+  const [profit, setProfit] = useState('0')
 
   useLayoutEffect(() => {
-    let itemsData= []
+    let itemsData = []
     const q = query(collection(db, 'agents'), where('associatedAccountUserId', '==', auth.currentUser.uid))
     const snapShotUnsubscribe = onSnapshot(q, updatedQuery => {
       itemsData = updatedQuery.docs.map(item => ({
@@ -53,23 +36,24 @@ const StatisticsScreen = () => {
       const dropdownResult = itemsData.map(x => ({
         label: x.agentName,
         value: {
+          name: x.agentName,
           apiKey: x.apiKey,
           apiSecret: x.apiSecret,
           dateCreated: x.dateOfCreation
         }
       }))
-      dropdownResult.splice(0, 0, { label: 'All', value: 'all' })
+      dropdownResult.splice(0, 0, { label: 'All agents', value: 'all' })
       setDropdownData(dropdownResult)
 
       dropdownResult.forEach(item => {
-        if (item.label == 'All') {
+        if (item.label == 'All agents') {
           return
         }
         TradesInformationFutures(item.value.dateCreated, item.value.apiKey, item.value.apiSecret,)
           .then((data) => {
             let trades = []
             data.forEach(trade => {
-              trades.push({[item.label]: trade})
+              trades.push({ [item.label]: trade })
             })
             setTradesData(trades)
           })
@@ -79,8 +63,49 @@ const StatisticsScreen = () => {
     return snapShotUnsubscribe
   }, [])
 
+  useLayoutEffect(() => {
+    function ChangeData() {
+      let profit = 0
+      let tempTrades = []
+      if (tradesData.length != 0) {
+        if (value == 'all') {
+          tradesData.forEach(item => {
+            const realizedProfit = Object.values(item).map(x => ({
+              timestamp: x.time,
+              value: x.realizedPnl
+            }))
+            realizedProfit.forEach(item => {
+              profit += parseFloat(item.value)
+              tempTrades.push(item)
+            })
+          })
+          setLineData(tempTrades)
+          setProfit(profit)
+        } else {
+          tradesData.forEach(item => {
+            const agentName = Object.keys(item)[0]
+            if (agentName == value.name) {
+              const realizedProfit = Object.values(item).map(x => ({
+                timestamp: x.time,
+                value: x.realizedPnl
+              }))
+              realizedProfit.forEach(item => {
+                profit += parseFloat(item.value)
+                tempTrades.push(item)
+              })
+            }
+          })
+          setLineData(tempTrades)
+          setProfit(profit)
+        }
+      }
+    }
+
+    ChangeData()
+  }, [tradesData, value])
+
   return (
-    <LineChart.Provider data={data}>
+    <LineChart.Provider data={lineData}>
       <View style={styles.container}>
         <Text style={styles.titleWrapper}>Agents statistics</Text>
         <View style={styles.chartWrapper}>
@@ -94,14 +119,17 @@ const StatisticsScreen = () => {
             style={[styles.dropdown, isFocus && { borderColor: 'blue' }]}
             placeholderStyle={styles.placeholderStyle}
             selectedTextStyle={styles.selectedTextStyle}
-            inputSearchStyle={styles.inputSearchStyle}
             iconStyle={styles.iconStyle}
             data={dropdownData}
             maxHeight={300}
             labelField="label"
             valueField="value"
-            placeholder={!isFocus ? 'Select item' : '...'}
+            placeholder={!isFocus ? 'All agents' : '...'}
             searchPlaceholder="Search..."
+            containerStyle={{ backgroundColor: 'black', borderRadius: 5 }}
+            itemContainerStyle={{ backgroundColor: 'black', borderRadius: 5 }}
+            activeColor='black'
+            itemTextStyle={{ color: 'white' }}
             value={value}
             onFocus={() => setIsFocus(true)}
             onBlur={() => setIsFocus(false)}
@@ -118,7 +146,7 @@ const StatisticsScreen = () => {
               />
             )}
           />
-          <Text style={styles.profitText}>Profit - </Text>
+          <Text style={styles.profitText}>Profit - {profit}</Text>
         </View>
       </View>
     </LineChart.Provider>
@@ -159,7 +187,7 @@ const styles = StyleSheet.create({
   dropdown: {
     height: 50,
     borderColor: 'gray',
-    borderWidth: 0.5,
+    borderWidth: 1,
     borderRadius: 8,
     paddingHorizontal: 8,
     width: '50%'
@@ -179,12 +207,10 @@ const styles = StyleSheet.create({
     width: 20,
     height: 20,
   },
-  inputSearchStyle: {
-    height: 40,
-    fontSize: 16,
-  },
   dropdownWrapper: {
     flexDirection: 'row',
-    alignItems: 'center'
+    alignItems: 'center',
+    marginLeft: '2%',
+    marginTop: '2%'
   }
 })
